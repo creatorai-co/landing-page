fd4676597ddd17484df72b4d40cb34389fec9a02
Jose K James <acreatoraico@gmail.com>
2025-01-01 21:14:47 +0530

    fix

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -4,9 +4,18 @@ import logo from "@/public/images/logo.svg";
 
 export default function Logo() {
   return (
-    <Link href="/" className="inline-flex shrink-0" aria-label="CreatorAI">
-      <Image src={logo} alt=" CreatorAI" width={32} height={32} />
-      {/* CreatorAI */}
+    <Link href="/" className="inline-flex items-center shrink-0" aria-label="CreatorAI">
+      <Image src={logo} alt="CreatorAI" width={32} height={32} />
+      <span
+        className="ml-2 animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.indigo.200),theme(colors.gray.50),theme(colors.indigo.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text font-nacelle text-2xl font-semibold text-transparent"
+      >
+        Creator
+        <span
+          className="ml-1 bg-gradient-to-r from-purple-500 to-violet-500 bg-clip-text text-transparent"
+        >
+          AI
+        </span>
+      </span>
     </Link>
   );
 }
